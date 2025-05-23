a6948f7db910051f6f6bde2a5d142532401bc69d
CMeloRuiz <cristhianmelo.ruiz@gmail.com>
2025-05-23 18:08:16 -0500

    primera version

---
+++ products.js
@@ -0,0 +1,380 @@
+const productsList = [
+    {
+        id: 1,
+        cat: "iluminacion",
+        img: "./Images/Products/tubo-led.png",
+        name: "TUBO LED 9W T8 VIDRIO 6500K",
+    },
+    {
+        id: 2,
+        cat: "iluminacion",
+        img: "./Images/Products/tubo-led.png",
+        name: "TUBO LED 18W T8 VIDRIO 6500K",
+    },
+    {
+        id: 3,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-gu10.png",
+        name: "BOMBILLO LED GU10 5W 3000K ",
+    },
+    {
+        id: 4,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-gu10.png",
+        name: "BOMBILLO  LED GU10 5W 6500K",
+    },
+    {
+        id: 5,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-gu10.png",
+        name: "BOMBILLO  LED GU10 6W 6500K",
+    },
+    {
+        id: 6,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-gu10.png",
+        name: "BOMBILLO  LED GU10 6W 3000K",
+    },
+    {
+        id: 7,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-gu10.png",
+        name: "BOMBILLO LED DIMERIZABLE GU10 7W 3000K",
+    },
+    {
+        id: 8,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-toledo1.png",
+        name: "BOMBILLO LED TOLEDO E27 9W 3000K",
+    },
+    {
+        id: 9,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-toledo1.png",
+        name: "BOMBILLO LED TOLEDO E27 9W 6500K",
+    },
+    {
+        id: 10,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-toledo1.png",
+        name: "BOMBILLO LED TOLEDO E27 12W 3000K",    
+    },
+    {
+        id: 11,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-toledo1.png",
+        name: "BOMBILLO LED TOLEDO E27 12W 6500K",   
+    },
+    {
+        id: 12,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-toledo2.png",
+        name: "BOMBILLO LED TOLEDO E27 20W 6500K",
+    },
+    {
+        id: 13,
+        cat: "iluminacion",
+        img: "./Images/Products/bombillo-led-toledo2.png",
+        name: "BOMBILLO LED TOLEDO E27 40W 6500K",
+    },
+    {
+        id: 14,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 3W 3000K",
+    },
+    {
+        id: 15,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 3W 6500K",
+    },
+    {
+        id: 16,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 6W 3000K",
+    },
+    {
+        id: 17,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 6W 6500K",    
+    },
+    {
+        id: 18,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 9W 3000K",
+    },
+    {
+        id: 19,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 9W 4000K",
+    },
+    {
+        id: 20,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 9W 6500K",    
+    },
+    {
+        id: 21,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 12W 3000K",
+    },
+    {
+        id: 22,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 12W 4000K",
+    },
+    {
+        id: 23,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 12W 6500K",
+    },
+    {
+        id: 24,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 18W 3000K",
+    },
+    {
+        id: 25,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 18W 4000K",    
+    },
+    {
+        id: 26,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 18W 6500K",    
+    },
+    {
+        id: 27,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 24W 3000K",    
+    },
+    {
+        id: 28,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 24W 4000K",    
+    },
+    {
+        id: 29,
+        cat: "iluminacion",
+        img: "./Images/Products/led-incrustar.png",
+        name: "PANEL LED DE INCRUSTAR 24W 6500K",    
+    },
+    {
+        id: 30,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 6W 6500K",    
+    },
+    {
+        id: 31,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 12W 3000K",    
+    },
+    {
+        id: 32,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 12W 4000K",    
+    },
+    {
+        id: 33,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 12W 6500K",    
+    },
+    {
+        id: 34,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 18W 3000K",    
+    },
+    {
+        id: 35,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 18W 4000K",    
+    },
+    {
+        id: 36,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 18W 6500K",    
+    },
+    {
+        id: 37,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 24W 3000K",    
+    },
+    {
+        id: 38,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 24W 4000K",   
+    },
+    {
+        id: 39,
+        cat: "iluminacion",
+        img: "./Images/Products/led-sobreponer.png",
+        name: "PANEL LED DE SOBREPONER 24W 6500K",    
+    },
+    {
+        id: 40,
+        cat: "iluminacion",
+        img: "./Images/Products/led-driver.png",
+        name: "LED DRIVER DE 40W  1000 MA",    
+    },
+    {
+        id: 41,
+        cat: "iluminacion",
+        img: "./Images/Products/led-cuadrado.png",
+        name: "PANEL LED CUADRADO 60*60 40W INCRUSTAR 4000K",    
+    },
+    {
+        id: 42,
+        cat: "iluminacion",
+        img: "./Images/Products/led-cuadrado.png",
+        name: "PANEL LED CUADRADO 60*60 40W INCRUSTAR 6500K",    
+    },
+    {
+        id: 43,
+        cat: "iluminacion",
+        img: "./Images/Products/led-rectangular.png",
+        name: "PANEL LED RECTANGULAR 120*30 40W INCRUSTAR 6500K",    
+    },
+    {
+        id: 44,
+        cat: "iluminacion",
+        img: "./Images/Products/lampara-emergencia-r1.png",
+        name: "LAMPARA DE EMERGENCIA R1 2X1.6W",    
+    },
+    {
+        id: 45,
+        cat: "iluminacion",
+        img: "./Images/Products/lampara-emergencia-r2.png",
+        name: "LAMPARA DE EMERGENCIA R2 3W",    
+    },
+    {
+        id: 46,
+        cat: "iluminacion",
+        img: "./Images/Products/lampara-emergencia-r3.png",
+        name: "LAMPARA DE EMERGENCIA R3 2X1W DESIGN",    
+    },
+    {
+        id: 47,
+        cat: "iluminacion",
+        img: "./Images/Products/lampara-hermetica.png",
+        name: "LAMPARA HERMETICA 2X18W T8 LUZ DIA",    
+    },
+    {
+        id: 48,
+        cat: "iluminacion",
+        img: "./Images/Products/lampara-tortuga.png",
+        name: "LAMPARA LED TORTUGA 12W LUZ DIA",    
+    },
+    {
+        id: 49,
+        cat: "iluminacion",
+        img: "./Images/Products/reflector-jeta.png",
+        name: "REFELECTOR LED JETA 10W LUZ DIA",    
+    },
+    {
+        id: 50,
+        cat: "iluminacion",
+        img: "./Images/Products/reflector-jeta.png",
+        name: "REFELECTOR LED JETA 50W LUZ DIA",    
+    },
+    {
+        id: 51,
+        cat: "iluminacion",
+        img: "./Images/Products/reflector-jeta.png",
+        name: "REFELECTOR LED JETA 100W LUZ DIA",    
+    },
+    {
+        id: 52,
+        cat: "iluminacion",
+        img: "./Images/Products/reflector-jeta.png",
+        name: "REFELECTOR LED JETA 200W LUZ DIA",    
+    },
+    {
+        id: 53,
+        cat: "ferreteria",
+        img: "./Images/Products/alicate-electrico.png",
+        name: "ALICATE ELECTRICO 8 84-023",    
+    },
+    {
+        id: 54,
+        cat: "ferreteria",
+        img: "./Images/Products/alicate-profesional.png",
+        name: "ALICATE PROFESIONAL",    
+    },
+    {
+        id: 55,
+        cat: "ferreteria",
+        img: "./Images/Products/alicate-aislado.png",
+        name: "ALICATE AISLADO 1000V 8 84-002",    
+    },
+    {
+        id: 56,
+        cat: "ferreteria",
+        img: "./Images/Products/destornillador-estrella.png",
+        name: "DESTORNILLADOR. STANLEY PROF. ESTR. 69-145 1/4 X 4",    
+    },
+    {
+        id: 57,
+        cat: "ferreteria",
+        img: "./Images/Products/destornillador-estrella.png",
+        name: "DESTORNILLADOR. STANLEY PROF. ESTR. 69-177 1/8 X 4",    
+    },
+    {
+        id: 58,
+        cat: "ferreteria",
+        img: "./Images/Products/destornillador-pala.png",
+        name: "DESTORNILLADOR. STANLEY PROF. PALA 69-104 1/8 X 4",    
+    },
+    {
+        id: 59,
+        cat: "ferreteria",
+        img: "./Images/Products/destornillador-pala.png",
+        name: "DESTORNILLADOR. STANLEY PROF. PALA 69-119 1/4 X 4",    
+    },
+    {
+        id: 60,
+        cat: "ferreteria",
+        img: "./Images/Products/tester-probador.png",
+        name: "TESTER STANLEY PROBADOR DESTORNILLADOR 66-119",    
+    },
+];
+
+
+// Ordenar la lista alfabeticamente
+
+productsList.sort((a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+    if (nameA < nameB) {
+        return -1;
+    };
+
+    if (nameA > nameB) {
+        return 1;
+    };
+
+    return 0;
+});
+
